dab5182df7ce442c5b337ebe9ad553e2216c80c3
Claudia Dominguez <126995375+CV17-09@users.noreply.github.com>
2025-12-02 12:40:03 -0600

    Delete WebProgProjectSocialMedia directory

--- WebProgProjectSocialMedia/collection.js
+++
@@ -1,9 +0,0 @@
-// Function to display the current local time
-function displayTime() {
-    const now = new Date();
-    const timeString = now.toLocaleString();
-    document.getElementById("time").textContent = timeString;
-}
-
-// Call the function when the page loads
-window.onload = displayTime;
